e1c6cd9831977db4e8c70b411ff76138872b3642
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-06-09 20:11:38 +0300

    Update Table.js

--- table/src/components/Table.js
+++ table/src/components/Table.js
@@ -1,213 +1,216 @@
-import React, { Component } from 'react'
-import TableRow from './TableRow'
-import TableSort from './TableSort';
-import TableHeader from './TableHeader';
-import TablePagination from './TablePagination';
-import { makeData } from '../makeRandomData';
+import React, { Component } from "react";
+import TableRow from "./TableRow";
+import TableSort from "./TableSort";
+import TableHeader from "./TableHeader";
+import TablePagination from "./TablePagination";
+import { makeData } from "../makeRandomData";
 
 function filterTable(tableContents, column, condition, input) {
-  const filteredTable = tableContents.filter(row => {
+  const filteredTable = tableContents.filter((row) => {
     let keepIt = true;
     let isValid = false;
-    let regEx = new RegExp(`${input}`, 'i')
-    
+    let regEx = new RegExp(`${input}`, "i");
+
     if (column && condition && input) {
-      if (column === 'name') {
-        if (condition === 'equals') {
-          keepIt = input.toLowerCase() === row[column].toLowerCase()
+      if (column === "name") {
+        if (condition === "equals") {
+          keepIt = input.toLowerCase() === row[column].toLowerCase();
         }
-        if (condition === 'contains') {
-          keepIt = regEx.test(row[column])
+        if (condition === "contains") {
+          keepIt = regEx.test(row[column]);
         }
       } else {
-        let number = Number(input)
+        let number = Number(input);
         isValid = !isNaN(number);
         if (isValid) {
-          if (condition === 'equals') {
-            keepIt = number === row[column]
+          if (condition === "equals") {
+            keepIt = number === row[column];
           }
-          if (condition === 'contains') {
-            keepIt = regEx.test(`${row[column]}`)
+          if (condition === "contains") {
+            keepIt = regEx.test(`${row[column]}`);
           }
-          if (condition === 'more') {
-            keepIt = row[column] > number
+          if (condition === "more") {
+            keepIt = row[column] > number;
           }
-          if (condition === 'less') {
-            keepIt = row[column] < number
+          if (condition === "less") {
+            keepIt = row[column] < number;
           }
         }
       }
     }
-    return keepIt
-  })
+    return keepIt;
+  });
 
   return filteredTable;
 }
 
 export default class Table extends Component {
-
   state = {
     isFiltered: false,
     pages: {
-      devidedIntoPages: [],
       currentPage: 0,
-      step: 10
+      step: 10,
     },
     filterValues: {
-      column: '',
-      condition: '',
-      input: '',
+      column: "",
+      condition: "",
+      input: "",
     },
-    tableHeader: ['date', 'name', 'quantity', 'distance'],
-    tableContents: []
-  }
+    tableHeader: ["date", "name", "quantity", "distance"],
+    tableContents: [],
+  };
 
   async componentDidMount() {
-    // асинхронно делаем запрос в базу данных
+    // асинхронно делаем запрос в базу данных await axios.get(...)
     // Если используем Redux, то это делаем через Thunk
     // и устанавливаем наш state
-    const tableContents = makeData(103)
+    try {
+      const tableContents = makeData(103);
 
-    this.setState({
-      tableContents,
-    })
+      this.setState({
+        tableContents,
+      });
+    } catch (error) {}
   }
 
   sortTable = (column) => {
-    if (column === 'id' || column === 'date') return;
+    if (column === "id" || column === "date") return;
     const contents = [...this.state.tableContents].sort((a, b) => {
-      if (column === 'name') {
-        if(a.name[0] < b.name[0]) return -1;
-        if(a.name[0] > b.name[0]) return 1;
+      if (column === "name") {
+        if (a.name[0] < b.name[0]) return -1;
+        if (a.name[0] > b.name[0]) return 1;
         return 0;
       }
       return a[column] - b[column];
-    })
+    });
     this.setState({
       tableContents: contents,
       pages: {
-        ...this.state.pages, currentPage: 0
+        ...this.state.pages,
+        currentPage: 0,
       },
-    })
-  }
+    });
+  };
 
   onColumnChange = (column) => {
     this.setState({
       isFiltered: true,
       filterValues: {
-        ...this.state.filterValues, column
-      }
-    })
-  }
+        ...this.state.filterValues,
+        column,
+      },
+    });
+  };
 
   onConditionChange = (condition) => {
     this.setState({
       isFiltered: true,
       filterValues: {
-        ...this.state.filterValues, condition
-      }
-    })
-  }
+        ...this.state.filterValues,
+        condition,
+      },
+    });
+  };
 
   onInputChange = (input) => {
     this.setState({
       isFiltered: true,
       filterValues: {
-        ...this.state.filterValues, input
+        ...this.state.filterValues,
+        input,
       },
       pages: {
-        ...this.state.pages, currentPage: 0
+        ...this.state.pages,
+        currentPage: 0,
       },
-    })
-    
-  }
+    });
+  };
 
   tableFilterHandler = () => {
-    
     this.setState({
-      isFiltered: true
-    })
-  }
+      isFiltered: true,
+    });
+  };
 
   onPageClickHandler = (event, currentPage) => {
-    event.preventDefault()
+    event.preventDefault();
     this.setState({
       pages: {
-        ...this.state.pages, currentPage
-      }
-    })
-  }
-  
+        ...this.state.pages,
+        currentPage,
+      },
+    });
+  };
+
   onPreviousPageClickHandler = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     this.setState({
       pages: {
-        ...this.state.pages, currentPage: this.state.pages.currentPage - 1
-      }
-    })
-  }
+        ...this.state.pages,
+        currentPage: this.state.pages.currentPage - 1,
+      },
+    });
+  };
 
   onNextPageClickHandler = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     this.setState({
       pages: {
-        ...this.state.pages, currentPage: this.state.pages.currentPage + 1
-      }
-    })
-  }
+        ...this.state.pages,
+        currentPage: this.state.pages.currentPage + 1,
+      },
+    });
+  };
 
+  render() {
+    // console.log(this.state);
 
-    render() {
-      // console.log(this.state);
-      
-      const {column, condition, input} = this.state.filterValues
-      const {currentPage, step} = this.state.pages
-      let tableContents = this.state.tableContents
-      if (this.state.isFiltered) {
-        tableContents = filterTable(tableContents, column, condition, input)
-      }
-        return (
-            <React.Fragment>
-
-              <TableSort
-                onColumnChange={this.onColumnChange}
-                onConditionChange={this.onConditionChange}
-                onInputChange={this.onInputChange}
-                tableFilter={this.tableFilterHandler}
-                inputDisabled={!(column && condition)}
-              />
-
-              <TablePagination
-                currentPage={currentPage}
-                pagesTotal={Math.ceil(tableContents.length / step)}
-                onPageClickHandler={this.onPageClickHandler}
-                onPreviousPageClickHandler={this.onPreviousPageClickHandler}
-                onNextPageClickHandler={this.onNextPageClickHandler}
-              />
-              
-              <table className='table table-striped'>
-                <tbody>
-                  <TableHeader
-                    tableHeader={this.state.tableHeader}
-                    sortTable={this.sortTable}
-                  />
-                    {
-                      tableContents.slice(currentPage * step, currentPage * step + step).map((rowProps) => {
-                        return <TableRow key={rowProps.id} {...rowProps}/>
-                      })
-                    }
-                </tbody>
-              </table>
-
-              <TablePagination
-                currentPage={currentPage}
-                pagesTotal={Math.ceil(tableContents.length / step)}
-                onPageClickHandler={this.onPageClickHandler}
-                onPreviousPageClickHandler={this.onPreviousPageClickHandler}
-                onNextPageClickHandler={this.onNextPageClickHandler}
-              />
-
-            </React.Fragment>
-        )
+    const { column, condition, input } = this.state.filterValues;
+    const { currentPage, step } = this.state.pages;
+    let tableContents = this.state.tableContents;
+    if (this.state.isFiltered) {
+      tableContents = filterTable(tableContents, column, condition, input);
     }
+    return (
+      <React.Fragment>
+        <TableSort
+          onColumnChange={this.onColumnChange}
+          onConditionChange={this.onConditionChange}
+          onInputChange={this.onInputChange}
+          tableFilter={this.tableFilterHandler}
+          inputDisabled={!(column && condition)}
+        />
+
+        <TablePagination
+          currentPage={currentPage}
+          pagesTotal={Math.ceil(tableContents.length / step)}
+          onPageClickHandler={this.onPageClickHandler}
+          onPreviousPageClickHandler={this.onPreviousPageClickHandler}
+          onNextPageClickHandler={this.onNextPageClickHandler}
+        />
+
+        <table className="table table-striped">
+          <tbody>
+            <TableHeader
+              tableHeader={this.state.tableHeader}
+              sortTable={this.sortTable}
+            />
+            {tableContents
+              .slice(currentPage * step, currentPage * step + step)
+              .map((rowProps) => {
+                return <TableRow key={rowProps.id} {...rowProps} />;
+              })}
+          </tbody>
+        </table>
+
+        <TablePagination
+          currentPage={currentPage}
+          pagesTotal={Math.ceil(tableContents.length / step)}
+          onPageClickHandler={this.onPageClickHandler}
+          onPreviousPageClickHandler={this.onPreviousPageClickHandler}
+          onNextPageClickHandler={this.onNextPageClickHandler}
+        />
+      </React.Fragment>
+    );
+  }
 }
